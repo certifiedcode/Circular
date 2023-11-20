136067d62ebafcec37023a232c70ea35c20d1b99
Wing <wing@certifiedcode.us>
2023-11-20 09:39:03 +0800

    Improvements

--- script.js
+++ script.js
@@ -1,47 +1,54 @@
+// <script src="https://cdn.jsdelivr.net/gh/peterhry/CircleType@2.3.1/dist/circletype.min.js"></script>
+// add the above script by js
+const script = document.createElement("script");
+script.src =
+  "https://cdn.jsdelivr.net/gh/peterhry/CircleType/dist/circletype.min.js";
+document.head.appendChild(script);
+
 window.addEventListener("message", (event) => {
-    if (event.data.circularId) {
-      // Get the element from the DOM
-      const element = document.getElementById(event.data.circularId)
-        .children[0];
+  if (event.data.circularId) {
+    // Get the element from the DOM
+    const element = document.getElementById(event.data.circularId)
+      .children[0];
 
-      // Create a new CircleType instance and set the radius
-      const circleType = new CircleType(element);
+    // Create a new CircleType instance and set the radius
+    const circleType = new CircleType(element);
 
-      // Use the template specified in the object data
-      switch (event.data.template) {
-        // case "basicArc":
-        //   circleType.radius(384);
-        //   break;
-        case "reversedArc":
-          circleType.dir(-1).radius(element.offsetWidth / 2);
-          window.addEventListener("resize", () => {
-            circleType.radius(element.offsetWidth / 2);
-          });
-          break;
-        case "autoRadius":
-          circleType.radius();
-          break;
-        case "basicArc":
+    // Use the template specified in the object data
+    switch (event.data.template) {
+      // case "basicArc":
+      //   circleType.radius(384);
+      //   break;
+      case "reversedArc":
+        circleType.dir(-1).radius(element.offsetWidth / 2);
+        window.addEventListener("resize", () => {
           circleType.radius(element.offsetWidth / 2);
-          window.addEventListener("resize", () => {
-            circleType.radius(element.offsetWidth / 2);
-          });
-          break;
-        // case "usingFitText":
-        //   circleType.radius(180);
-        //   $(element).fitText();
-        //   break;
-        // case "destroy":
-        //   circleType.destroy();
-        //   break;
-        // case "emojis":
-        //   // Use a GraphemeSplitter to split emojis correctly
-        //   const splitter = new GraphemeSplitter();
-        //   circleType = new CircleType(
-        //     element,
-        //     splitter.splitGraphemes.bind(splitter)
-        //   );
-        //   break;
-      }
+        });
+        break;
+      case "autoRadius":
+        circleType.radius();
+        break;
+      case "basicArc":
+        circleType.radius(element.offsetWidth / 2);
+        window.addEventListener("resize", () => {
+          circleType.radius(element.offsetWidth / 2);
+        });
+        break;
+      // case "usingFitText":
+      //   circleType.radius(180);
+      //   $(element).fitText();
+      //   break;
+      // case "destroy":
+      //   circleType.destroy();
+      //   break;
+      // case "emojis":
+      //   // Use a GraphemeSplitter to split emojis correctly
+      //   const splitter = new GraphemeSplitter();
+      //   circleType = new CircleType(
+      //     element,
+      //     splitter.splitGraphemes.bind(splitter)
+      //   );
+      //   break;
     }
-  });
+  }
+});
